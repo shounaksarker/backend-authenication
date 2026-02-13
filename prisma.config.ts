df1e73bbadf421e0c699a5b88ff5ceae48bd4cf9
shounak.sarker <shounaksarker@gmail.com>
2026-02-13 13:10:29 +0600

    fix: db_url from env

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-const db_user = env("DATABASE_USER");
-const db_password = env("DATABASE_PASSWORD");
-const db_host = env("DATABASE_HOST");
-const db_port = env("DATABASE_PORT");
-const db_name = env("DATABASE_NAME");
+const db_user = env("DATABASE_USER") || "root";
+const db_password = env("DATABASE_PASSWORD") || "root";
+const db_host = env("DATABASE_HOST") || "localhost";
+const db_port = env("DATABASE_PORT") || "3306";
+const db_name = env("DATABASE_NAME") || "auth_db";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
